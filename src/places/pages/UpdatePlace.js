c7718f717a64a793e933a3b5870256b1d10c9e5a
Jeorge Santos <jemjem.santos03@gmail.com>
2023-05-02 03:32:28 +0800

    updating place done

--- src/places/pages/UpdatePlace.js
+++ src/places/pages/UpdatePlace.js
@@ -1,38 +1,32 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useParams } from 'react-router-dom';
-
 import Input from '../../shared/components/FormElements/Input';
 import Button from '../../shared/components/FormElements/Buttons';
 import {
   VALIDATOR_MINLENGTH,
   VALIDATOR_REQUIRE,
 } from '../../shared/util/validators';
-
 import { useForm } from '../../shared/hooks/form-hook';
 import { useHttpRequest } from '../../shared/hooks/httpRequestHook';
 import Spinner from '../../shared/components/loading/Spinner';
 import ErrorModal from '../../shared/components/UIElements/ErrorModal';
 import api from '../../api/server';
 import { getPlace } from '../../api/getPlace';
-
 import './PlaceForm.css';
-
 let runSetFormData = false;
 
 const UpdatePlace = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
   const [place, setPlace] = useState({});
-  console.log(place);
 
   const placeId = useParams().placeId;
-
-  console.log(placeId);
 
   const [formState, inputHandler, setFormData] = useForm(
     {
       title: { value: '', isValid: false },
       description: { value: '', isValid: false },
     },
-    false
+    true
   );
 
   const { isLoading, error, errorReset, sendRequest } = useHttpRequest();
@@ -42,14 +36,13 @@ const UpdatePlace = () => {
       try {
         const response = await getPlace(placeId);
 
-        console.log(response);
-        console.log('get placece');
         setPlace(response);
         runSetFormData = true;
       } catch (err) {
         console.log(err);
       }
     };
+
     if (runSetFormData) {
       setFormData(
         {
@@ -58,8 +51,6 @@ const UpdatePlace = () => {
         },
         true
       );
-
-      console.log('formdata');
     }
     sendRequest();
   }, [runSetFormData, setPlace, setFormData]);
@@ -87,47 +78,64 @@ const UpdatePlace = () => {
     const description = formState.inputs.description.value;
 
     try {
-      const response = await sendRequest(`/api/places/${placeId}`, api.post, {
+      const response = await sendRequest(`/api/places/${placeId}`, api.patch, {
         title,
         description,
       });
-    } catch (err) {}
+
+      // TIMER FOR ALERTS
+      setIsSubmitted(true);
+      setTimeout(() => {
+        setIsSubmitted(false);
+      }, 5000);
+    } catch (err) {
+      // TO LET THE DEVELOPER OR USER THE ERROR
+      console.log(err);
+    }
   };
 
   return (
-    <form onSubmit={updatePlaceHandler} className="place-form">
-      {place.description || place.title ? (
-        <>
-          <Input
-            id="title"
-            element="input"
-            label="Title"
-            validators={[VALIDATOR_REQUIRE()]}
-            errorText={'Please enter a valid title'}
-            onChange={inputHandler}
-            initialValue={place.title}
-            valid={formState.inputs.title.isValid}
-          />
+    <>
+      {/* ERROR MODAL */}
+      <ErrorModal show={error} onCancel={errorReset}>
+        {error}
+      </ErrorModal>
+      <form onSubmit={updatePlaceHandler} className="place-form">
+        {/* ALERT IF FORM IS SENT SUCCESSFUL */}
+        {isSubmitted && !isLoading ? (
+          <p className="success-alert text-center"> Place updated!</p>
+        ) : null}
+        {place.description || place.title ? (
+          <>
+            <Input
+              id="title"
+              element="input"
+              label="Title"
+              validators={[VALIDATOR_REQUIRE()]}
+              errorText={'Please enter a valid title'}
+              onChange={inputHandler}
+              valid={formState.inputs.title.isValid}
+            />
 
-          <Input
-            id="description"
-            element="textarea"
-            label="Description"
-            validators={[VALIDATOR_MINLENGTH(5)]}
-            errorText={'Please Enter a valid description'}
-            onChange={inputHandler}
-            initialValue={place.description}
-            valid={formState.inputs.description.isValid}
-          />
-        </>
-      ) : (
-        <Spinner />
-      )}
+            <Input
+              id="description"
+              element="textarea"
+              label="Description"
+              validators={[VALIDATOR_MINLENGTH(5)]}
+              errorText={'Please Enter a valid description'}
+              onChange={inputHandler}
+              valid={formState.inputs.description.isValid}
+            />
+          </>
+        ) : (
+          <Spinner />
+        )}
 
-      <Button type={'submit'} disabled={!formState.isValid}>
-        Update place
-      </Button>
-    </form>
+        <Button type={'submit'} disabled={!formState.isValid}>
+          Update place
+        </Button>
+      </form>
+    </>
   );
 };
 
