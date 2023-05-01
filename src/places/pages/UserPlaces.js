d56907a26963bc7473fb6a8d4f6257ef5ffab8e6
Jeorge Santos <jemjem.santos03@gmail.com>
2023-05-02 07:18:39 +0800

    fixed some bugs

--- src/places/pages/UserPlaces.js
+++ src/places/pages/UserPlaces.js
@@ -1,22 +1,24 @@
 import React, { useContext, useEffect, useState } from 'react';
 import PlaceList from '../components/PlaceList';
-import { useParams } from 'react-router-dom';
 import { getUserPlace } from '../../api/getUserPlaces';
 
 import { userContext } from '../../shared/context/user-context';
+import { useParams } from 'react-router-dom';
 
 const UserPlaces = () => {
   const [userPlaces, setUserPlaces] = useState([]);
   const userId = useContext(userContext).id;
+  const uId = useParams().userId;
 
   useEffect(() => {
     const getPlaces = async () => {
-      const response = await getUserPlace(userId);
+      const response = await getUserPlace(uId);
+      console.log(response);
 
       setUserPlaces(response);
     };
     getPlaces();
-  }, [setUserPlaces, userId, userPlaces]);
+  }, [setUserPlaces, userId]);
 
   return <PlaceList items={userPlaces} />;
 };
